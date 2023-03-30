19f631280f335d767d8cb8e30e0c549da0b182e6
NahuelBergallo <pnahuelbergallo2001@gmail.com>
2023-03-30 00:53:14 -0300

    Este es el modelo de primer contar mio

---
+++ contarmodel1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -ne "loremipsun-1.txt tiene 8 lineas"
+sleep 1s
+
+echo -ne "loremipsun-2.txt tiene 20 lineas"
+sleep 1s
+
+echo -ne "loremipsun-3.txt tiene 6 lineas"
+sleep 1s
+
+echo -ne "loremipsun-4.txt tiene 14 lineas"
+sleep 1s
+
+echo -ne "loremipsun-5.txt tiene 20 lineas"
+sleep 1s
+
+echo -ne "/n"
